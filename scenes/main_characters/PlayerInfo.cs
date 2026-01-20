b5de52d4e7ab6318236ee1998a21991f94a3dcd5
Fotis <f.grammenos98@gmail.com>
2026-01-20 15:19:33 +0200

    Add Co-Op

---
+++ scenes/main_characters/PlayerInfo.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+public class PlayerInfo
+{
+    public int Id {get; set;}
+    public string Name {get; set;}
+    public int Points {get; set;}
+    public string SelectedCharacterPath {get; set;}
+}
